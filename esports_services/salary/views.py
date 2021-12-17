f5b194ac268eb35d8ce3ba46acc874f1f43c3114
Sergey K <skapkanshchikov@gmail.com>
2021-12-17 21:38:58 +0300

    Add profile data to template

--- esports_services/salary/views.py
+++ esports_services/salary/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.http import request
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.contrib.auth import logout
+from django.contrib.auth import get_user_model, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
+
+import datetime
 
 
 def registration(request):
@@ -33,6 +36,7 @@ def registration(request):
 class LoginUser(LoginView):
     template_name = 'salary/login.html'
     form_class = AuthenticationForm
+    redirect_authenticated_user = True
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -50,8 +54,50 @@ class IndexView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Личный кабинет'
-        print(context)
+        context['experience'] = self.get_work_experience()
         return context
+
+    def get_work_experience(self):
+        employment_date = User.objects.get(username=self.request.user).profile.employment_date
+        experience = datetime.date.today() - employment_date
+        days = experience.days
+        experience_text = ''
+
+        if days > 365:
+            years = days // 365
+            days = days % 365
+            experience_text = str(years)
+
+            if years == 1:
+                experience_text += ' год '
+            elif 1 < years < 5:
+                experience_text += ' года '
+            else:
+                experience_text += ' лет '
+
+        if days > 30:
+            month = days // 30
+            days = days % 30
+            experience_text += str(month) + ' месяц'
+
+            if month == 1:
+                experience_text += ' '
+            if 1 < month < 5:
+                experience_text += 'а '
+            else:
+                experience_text += 'ев '
+
+        experience_text += str(days) + ' д'
+        end_num = days % 10
+
+        if end_num == 1:
+            experience_text += 'ень'
+        elif 1 < end_num < 5:
+            experience_text += 'ня'
+        else:
+            experience_text += 'ней'
+
+        return experience_text
 
     def get_success_url(self, **kwargs):
         return reverse_lazy('index')
