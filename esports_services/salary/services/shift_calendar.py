29dbdda0db80407127b4df759470ddecb6937f7f
Sergey K <skapkanshchikov@gmail.com>
2022-07-29 17:53:36 +0300

    Change calc algorithm. Add counts ans sum.

--- esports_services/salary/services/shift_calendar.py
+++ esports_services/salary/services/shift_calendar.py
@@ -10,8 +10,15 @@ from salary.models import WorkingShift
 from .utils import time_of_run
 
 
-CalendarDay = namedtuple('CalendarDay', ['date', 'earnings', 'is_planed'])
-
+CalendarDay = namedtuple(
+    'CalendarDay',
+    ['date', 'earnings', 'is_planed', 'link']
+)
+UserCalendar = namedtuple(
+    'UserCalendar',
+    ['weeks_list', 'complited_shifts_count',
+     'all_shifts_count', 'sum_of_earnings']
+)
 
 def get_week_days_list(year: int = None, month: int = None) -> List[List]:
     """
@@ -51,8 +58,7 @@ def get_user_month_workshifts(user: User, date: datetime.date) -> QuerySet:
 
 
 @time_of_run
-def get_user_calendar(user: User,
-                        date: datetime.date) -> List[List[CalendarDay]]:
+def get_user_calendar(user: User, date: datetime.date) -> UserCalendar:
     """
     Return list of lists of namedtuples with days of month.
     """
@@ -60,42 +66,64 @@ def get_user_calendar(user: User,
     import random
     planed_workshifts = [random.randrange(1, 30) for _ in range(1, 5)]
 
-    workshifts = get_user_month_workshifts(user, date)
+    workshifts: QuerySet = get_user_month_workshifts(user, date)
+    
+    workshift_tuples_list = []
+    sum_of_earnings: float = 0
+    for workshift in workshifts:
+        if workshift.hall_admin == user:
+            earnings = workshift.hall_admin_earnings_calc()
+        else:
+            earnings = workshift.cashier_earnings_calc()
+        current_earnings = earnings.get('final_earnings')
+        sum_of_earnings += current_earnings
+        workshift_tuples_list.append(
+            CalendarDay(
+                date=workshift.shift_date,
+                earnings=current_earnings,
+                is_planed=False,
+                link=workshift.get_absolute_url()
+            )
+        )
+    complited_shifts_count = len(workshift_tuples_list)
+    all_shifts_count = complited_shifts_count + len(planed_workshifts)
+    
     shift_dates_list = workshifts.dates('shift_date', 'day')
-    weeks_list = get_week_days_list(date.year, date.month)
-    user_calendar = []
-    for week in weeks_list:
-        week_tuples_list = []
+
+    weeks_days_list = get_week_days_list(date.year, date.month)
+    calendar_weeks_list = []
+    for week in weeks_days_list:
+        day_tuples_list = []
         for day in week:
             if day == 0:
-                week_tuples_list.append(None)
+                day_tuples_list.append(None)
             else:
                 date_of_day = datetime.date(date.year, date.month, day)
                 if date_of_day in shift_dates_list:
-                    workshift = workshifts.get(shift_date=date_of_day)
-                    earnings = {}
-                    if workshift.hall_admin == user:
-                        earnings = workshift.hall_admin_earnings_calc()
-                    else:
-                        earnings = workshift.cashier_earnings_calc()
-                        
-                    day_tuple = CalendarDay(
-                        date=date_of_day,
-                        earnings=earnings.get('final_earnings'),
-                        is_planed = False
-                    )
+                    day_tuple = [
+                        day for day in workshift_tuples_list 
+                        if day.date == date_of_day
+                    ][0]
                 elif date_of_day.day in planed_workshifts:
                     day_tuple = CalendarDay(
                         date=date_of_day,
                         earnings=None,
-                        is_planed=True
+                        is_planed=True,
+                        link=None
                     )
                 else:
                     day_tuple = CalendarDay(
                         date=date_of_day,
                         earnings=None,
-                        is_planed=False
+                        is_planed=False,
+                        link=None
                     )
-                week_tuples_list.append(day_tuple)
-        user_calendar.append(week_tuples_list)
+                day_tuples_list.append(day_tuple)
+        calendar_weeks_list.append(day_tuples_list)
+    user_calendar = UserCalendar(
+        weeks_list=calendar_weeks_list,
+        complited_shifts_count=complited_shifts_count,
+        all_shifts_count=all_shifts_count,
+        sum_of_earnings=round(sum_of_earnings, 2)
+    )
     return user_calendar
