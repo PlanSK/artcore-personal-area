2711fa7156f8ac7c9e3086645191522c4771bed1
PlanSK <plansk@artcore24.ru>
2023-02-18 01:05:29 +0300

    Refactoring user calendar with dates, Enum and dataclass.

--- salary/services/shift_calendar.py
+++ salary/services/shift_calendar.py
@@ -2,6 +2,8 @@ import calendar
 import datetime
 import logging
 
+from dataclasses import dataclass
+from enum import Enum
 from typing import List, NamedTuple
 
 from django.db.models import QuerySet
@@ -16,12 +18,27 @@ from salary.services.db_orm_queries import (
 )
 
 
-class CalendarDay(NamedTuple):
+class DayStatus(Enum):
+    FOREIGN = 1
+    REGULAR = 2
+    PLANED = 3
+    UNVERIFIED = 4
+    VERIFIED = 5
+
+
+@dataclass
+class CalendarDay:
     date: datetime.date
+    status: DayStatus = DayStatus.REGULAR
+    earnings: float = 0.0
+    url: str = ''
+
+
+class Workshift(NamedTuple):
+    shift_date: datetime.date
     earnings: float
-    is_planed: bool
     is_verified: bool
-    link: str
+    url: str
 
 
 class UserCalendar(NamedTuple):
@@ -39,7 +56,7 @@ class EmployeeOnWork(NamedTuple):
 logger = logging.getLogger(__name__)
 
 
-def get_week_days_list(year: int, month: int) -> List[List]:
+def get_month_calendar(year: int, month: int) -> list[list[datetime.date]]:
     """
     Return a matrix representing a month's calendar.
     Each row represents a week; week entries are
@@ -47,101 +64,44 @@ def get_week_days_list(year: int, month: int) -> List[List]:
     are zero.
     """
     calendar_instance = calendar.Calendar(firstweekday=0)
+    month_calender = []
     try:
-        month_calender = calendar_instance.monthdayscalendar(year=year,
+        month_calender = calendar_instance.monthdatescalendar(year=year,
                                                              month=month)
     except calendar.IllegalMonthError:
-        logger.exception(f'Bad month number. {exception}')
-        raise TypeError(
+        logger.exception(
             'Error in arguments type monthdayscalendar(). Bad month number.')
     except TypeError as exception:
         logger.exception(
             f'Error in arguments type monthdayscalendar(): {exception}')
-        raise
 
     return month_calender
 
 
-def get_workshift_tuples_list(user_id: int,
-                              year: int,
-                              month: int) -> List[CalendarDay]:
-    """
-    Return list of CalendarDay namedtuple from workshifts data.
-    """
-
-    workshifts: QuerySet = get_user_month_workshifts(
-        user_id=user_id,
-        year=year,
-        month=month
-    )
-
-    workshift_tuples_list = []
+def get_workshift_dict(user_id: int, year: int, month: int) -> dict:
+    """Return list of CalendarDay namedtuple from workshifts data."""
+    workshifts = get_user_month_workshifts(user_id=user_id, year=year,
+                                           month=month)
+    workshift_dict = dict()
     for workshift in workshifts:
+        earnings = workshift.cashier_earnings
+        is_verified = False
         if workshift.hall_admin.pk == user_id:
             earnings = workshift.hall_admin_earnings
-        else:
-            earnings = workshift.cashier_earnings
-        current_earnings: float = earnings.final_earnings
-        displayed_date = workshift.shift_date - datetime.timedelta(days=1)
-        if workshift.shift_date == datetime.date(year, month, 1):
-            displayed_date = workshift.shift_date
-        is_verified = True if WorkingShift.WorkshiftStatus.VERIFIED else False
-        workshift_tuples_list.append(
-            CalendarDay(
-                date=displayed_date,
-                earnings=current_earnings,
-                is_planed=False,
-                is_verified=is_verified,
-                link=workshift.get_absolute_url()
-            )
+        if workshift.status == WorkingShift.WorkshiftStatus.VERIFIED:
+            is_verified = True
+        workshift_url = workshift.get_absolute_url()
+        current_workshift = Workshift(
+            shift_date=workshift.shift_date,
+            earnings=earnings.final_earnings,
+            is_verified=is_verified,
+            url=workshift_url
         )
-    return workshift_tuples_list
+        workshift_dict.update({
+            workshift.shift_date: current_workshift
+        })
 
-
-def get_calendar_weeks_list(
-    weeks_days_list: List[List[int]],
-    shift_dates_list: List[datetime.date],
-    year: int, month: int,
-    workshift_tuples_list: List[CalendarDay],
-    planed_workshifts_list: List[datetime.date]
-    ) -> List[List[CalendarDay]]:
-    """
-    Returns List[List[CalendarDay]] - list of lists with CalendarDay
-    """
-
-    calendar_weeks_list = []
-    for week in weeks_days_list:
-        day_tuples_list = []
-        for day in week:
-            if day == 0:
-                day_tuples_list.append(None)
-            else:
-                date_of_day = datetime.date(year, month, day)
-                if date_of_day in shift_dates_list:
-                    day_tuple = [
-                        day for day in workshift_tuples_list 
-                        if day.date == date_of_day
-                    ][0]
-                elif date_of_day in planed_workshifts_list:
-                    day_tuple = CalendarDay(
-                        date=date_of_day,
-                        earnings=None,
-                        is_planed=True,
-                        is_verified=False,
-                        link=None
-                    )
-                else:
-                    day_tuple = CalendarDay(
-                        date=date_of_day,
-                        earnings=None,
-                        is_planed=False,
-                        is_verified=False,
-                        link=None
-                    )
-                day_tuples_list.append(day_tuple)
-        calendar_weeks_list.append(day_tuples_list)
-
-    return calendar_weeks_list
+    return workshift_dict
 
 
 def get_planed_workshifts_days_list(user_id: int, month: int,
@@ -154,6 +114,30 @@ def get_planed_workshifts_days_list(user_id: int, month: int,
     return employee_days_list
 
 
+def get_calendar_week_list(
+        week: list[datetime.date], planed_workshifts_list: list[datetime.date],
+        workshift_dict: dict, month: int) -> list:
+    """Returns week list with CalendarDay's."""
+    calendar_week_list = []
+    for day_date in week:
+        current_day = CalendarDay(day_date)
+        tomorow = day_date + datetime.timedelta(days=1)
+        if day_date.month != month:
+            current_day.status = DayStatus.FOREIGN
+        if (day_date in planed_workshifts_list 
+                and tomorow not in workshift_dict.keys()):
+            current_day.status = DayStatus.PLANED
+        if day_date in workshift_dict.keys():
+            current_workshift: Workshift = workshift_dict.get(day_date)
+            current_day.earnings = current_workshift.earnings
+            current_day.status = DayStatus.UNVERIFIED
+            if current_workshift.is_verified:
+                current_day.status = DayStatus.VERIFIED
+            current_day.url = current_workshift.url
+        calendar_week_list.append(current_day)
+    return calendar_week_list
+
+
 def get_user_calendar(user_id: int, year: int, month: int) -> UserCalendar:
     """
     Return UserCalendar namedtuple.
@@ -164,49 +148,27 @@ def get_user_calendar(user_id: int, year: int, month: int) -> UserCalendar:
     """
 
     planed_workshifts_list = get_planed_workshifts_days_list(
-        user_id=user_id,
-        year=year,
-        month=month
-    )
+        user_id=user_id, year=year, month=month)
+    workshift_dict = get_workshift_dict(user_id=user_id, year=year,
+                                        month=month)
+    month_calendar = get_month_calendar(year=year, month=month)
+    calendar_days_month_list = []
 
-    workshift_tuples_list = get_workshift_tuples_list(
-        user_id=user_id,
-        year=year,
-        month=month
-    )
+    for week in month_calendar:
+        calendar_week_list = get_calendar_week_list(
+            week, planed_workshifts_list, workshift_dict, month)
+        calendar_days_month_list.append(calendar_week_list)
 
-    shift_dates_list: list = [shift.date for shift in workshift_tuples_list]
-
-    weeks_days_list = get_week_days_list(year=year, month=month)
-
-    calendar_weeks_list = get_calendar_weeks_list(
-        weeks_days_list,
-        shift_dates_list=shift_dates_list,
-        year=year,
-        month=month,
-        workshift_tuples_list=workshift_tuples_list,
-        planed_workshifts_list=planed_workshifts_list
-    )
-
-    complited_shifts_count: int = 0
-    planed_shifts_count: int = 0
-    sum_of_earnings: float = 0.0
-
-    for week in calendar_weeks_list:
-        for day in week:
-            if day:
-                if day.earnings:
-                    complited_shifts_count += 1
-                    if day.is_verified:
-                        sum_of_earnings += day.earnings
-                elif day.is_planed:
-                    planed_shifts_count += 1
-
-    if not planed_workshifts_list:
-        planed_shifts_count = -1
+    complited_shifts_count = len(workshift_dict.keys())
+    planed_shifts_count = len(planed_workshifts_list)
+    sum_of_earnings = sum([
+        workshift.earnings
+        for workshift in workshift_dict.values()
+        if workshift.is_verified
+    ])
 
     user_calendar = UserCalendar(
-        weeks_list=calendar_weeks_list,
+        weeks_list=calendar_days_month_list,
         complited_shifts_count=complited_shifts_count,
         planed_shifts_count=planed_shifts_count,
         sum_of_earnings=round(sum_of_earnings, 2)
@@ -220,7 +182,7 @@ def get_employees_at_work() -> EmployeeOnWork:
     now = timezone.localtime(timezone.now())
     today = now.date()
     if now.hour < settings.EMPLOYEE_CHANGE_HOUR:
-        today = now().date() - datetime.timedelta(days=1)
+        today -= datetime.timedelta(days=1)
     current_month_employee_planed_shifts = get_employees_schedule_dict(
         year=today.year, month=today.month)
     employee_list = [
